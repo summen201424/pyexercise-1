3560918ec2d1901bee15fa24bd6f9b1040555896
609692497@qq.com <609692497@qq.com>
2018-12-12 17:44:37 +0800

    #第四次递交

--- venv/totalexercise/wx.py
+++ venv/totalexercise/wx.py
@@ -1,8 +1,11 @@
 import itchat
 from helloworld import getResponse
-@itchat.msg_register(itchat.content.TEXT)
-def text_reply(msg):
-    return getResponse(msg["Text"])["text"]
-itchat.auto_login(hotReload=True)
-itchat.run()
-input()
+# @itchat.msg_register(itchat.content.TEXT)
+# def text_reply(msg):
+#     return getResponse(msg["Text"])["text"]
+# itchat.auto_login(hotReload=True)
+# itchat.run()
+# input()
+dict1={"a":1,"b":2}
+print(dict1)
+1111
